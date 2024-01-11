0b2678fd5ec018ec8120c99a37c7bf0f87152584
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-11 18:07:30 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -36,6 +37,12 @@ public class Program {
                 
                 if(capturedPiece != null) {
                     captured.add(capturedPiece);
+                }
+                
+                if(chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
                 }
             }
             catch(ChessException e) {
